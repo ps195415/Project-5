6b8e895bdc392163da802beb7f8d1beb6b124c0b
ps195415 <ps195415@summacollege.nl>
2022-06-20 11:40:18 +0200

    Niet veilig oefeneningen get gemaakt

--- SummaMoveApi/routes/api.php
+++ SummaMoveApi/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\GuestOefeningenControl;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('Guest', [GuestOefeningenControl::class,'index']);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
